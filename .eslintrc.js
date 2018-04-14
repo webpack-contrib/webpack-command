ab6f34302ea35ec450f2fc5bdcdd68e90d801cd6
shellscape <andrew@shellscape.org>
2018-04-14 09:24:22 -0400

    refactor: workout out the plugin command system

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: ['prettier'],
   extends: ['@webpack-contrib/eslint-config-webpack'],
   rules: {
+    'class-methods-use-this': 'off',
     'prettier/prettier': [
       'error',
       { singleQuote: true, trailingComma: 'es5', arrowParens: 'always' },
