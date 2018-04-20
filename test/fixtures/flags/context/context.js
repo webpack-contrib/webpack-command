229b2e27001f79cff860b122e88a2dd0ad8d309b
shellscape <andrew@shellscape.org>
2018-04-20 17:18:07 -0400

    test: add context flag fixtures

---
+++ test/fixtures/flags/context/context.js
@@ -0,0 +1,13 @@
+const { resolve } = require('path');
+
+const config = require('../../common/webpack.config');
+
+const context = resolve(__dirname, '../../common');
+
+module.exports = {
+  arguments: ['--context', context],
+
+  config: Object.assign(config, {
+    entry: './entry-a.js',
+  }),
+};
