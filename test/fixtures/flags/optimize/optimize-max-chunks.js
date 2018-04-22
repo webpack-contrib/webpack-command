0efbdf11ae0ce85e22d710f69abddc7dcfcef728
shellscape <andrew@shellscape.org>
2018-04-22 16:42:30 -0400

    test: add optimize test fixtures

---
+++ test/fixtures/flags/optimize/optimize-max-chunks.js
@@ -0,0 +1,15 @@
+const { resolve } = require('path');
+
+const config = require('../../common/webpack.config');
+
+module.exports = {
+  arguments: ['--optimize-max-chunks', '1000'],
+
+  config: Object.assign(config, {
+    entry: [
+      resolve(__dirname, '../../common/entry-a.js'),
+      resolve(__dirname, '../../common/entry-b.js'),
+      resolve(__dirname, '../../common/entry-c.js'),
+    ],
+  }),
+};
