fa82cb5ad5564e293a0695f9d02d0bc43dab4846
shellscape <andrew@shellscape.org>
2018-04-27 22:30:24 -0400

    test: adding more cli tests, removing dead code

--- test/tests/cli.js
+++ test/tests/cli.js
@@ -34,7 +34,7 @@ test('Zero Config', module, () => {
 
 test('Commands', module, () => {
   it('should show cli help', () => {
-    const cliPath = resolve(__dirname, '../../lib/cli');
+    const cliPath = resolve(__dirname, '../../lib/cli.js');
     const result = execa.sync('node', [cliPath, '--help']);
 
     expect(
@@ -42,10 +42,41 @@ test('Commands', module, () => {
     ).toMatchSnapshot();
   });
 
-  it('should show help command help', () => {
-    const cliPath = resolve(__dirname, '../../lib/cli');
-    const result = execa.sync('node', [cliPath, 'help', 'teach']);
+  it('should show teach command help', () => {
+    const cliPath = resolve(__dirname, '../../lib/cli.js');
+    const result = execa.sync(cliPath, ['help', 'teach']);
 
     expect(result.stdout.replace(/v\d\.\d\.\d/, '<version>')).toMatchSnapshot();
-  });
+  }).timeout(4000);
+
+  it('should throw on command error', () => {
+    const cliPath = resolve(__dirname, '../../lib/cli.js');
+    const stub = () => execa.sync(cliPath, ['help', 'bad-command']);
+
+    // TODO: move to error codes
+    expect(stub).toThrow(/'bad-command' has not been installed/);
+  }).timeout(4000);
+
+  it('should throw on missing command', () => {
+    const cliPath = resolve(__dirname, '../../lib/cli.js');
+    const stub = () =>
+      execa.sync(cliPath, ['bad-command'], {
+        env: {
+          CLI_TEST: 'true',
+        },
+      });
+
+    // TODO: move to error codes
+    expect(stub).toThrow(/bad-command/);
+  }).timeout(4000);
+
+  it('should add directories to entries', () => {
+    const cliPath = resolve(__dirname, '../../lib/cli.js');
+    const srcPath = resolve(__dirname, '../../test/fixtures/flags/config/src');
+    const result = execa.sync(cliPath, [srcPath]);
+
+    expect(
+      strip(result.stdout).replace(/Δt \d+ms/g, '<duration>')
+    ).toMatchSnapshot();
+  }).timeout(4000);
 });
